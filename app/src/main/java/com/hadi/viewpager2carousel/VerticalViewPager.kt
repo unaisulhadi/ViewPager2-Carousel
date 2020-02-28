80ec2328f3a3f26a6978f16fb90ad79bd5e26b42
Unaisul Hadi <unaisulhadi@gmail.com>
2020-02-28 18:18:52 +0530

    Added Vertcal

--- app/src/main/java/com/hadi/viewpager2carousel/VerticalViewPager.kt
+++ app/src/main/java/com/hadi/viewpager2carousel/VerticalViewPager.kt
@@ -1,29 +1,27 @@
 package com.hadi.viewpager2carousel
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.viewpager2.widget.ViewPager2
-import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.Math.abs
+import kotlinx.android.synthetic.main.activity_vertical_view_pager.*
 
-class MainActivity : AppCompatActivity() {
+class VerticalViewPager : AppCompatActivity() {
 
     var modelList = arrayListOf<ModelData>()
     lateinit var adapter: ModelAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_vertical_view_pager)
 
         loadData()
 
-        vertical.setOnClickListener {
-            startActivity(Intent(this,VerticalViewPager::class.java))
+        horizontal.setOnClickListener {
+            finish()
         }
-
     }
+
 
     private fun loadData() {
         modelList.add(ModelData("Name 1","22-05-2019","https://i.picsum.photos/id/415/500/500.jpg"))
@@ -35,25 +33,27 @@ class MainActivity : AppCompatActivity() {
         setupCarousel()
 
         adapter = ModelAdapter(this,modelList)
-        viewPager.adapter = adapter
-
+        viewPagervertical.adapter = adapter
     }
+
 
     fun setupCarousel(){
 
-        viewPager.offscreenPageLimit = 1
+
+
+        viewPagervertical.offscreenPageLimit = 1
 
         val nextItemVisiblePx = resources.getDimension(R.dimen.viewpager_next_item_visible)
         val currentItemHorizontalMarginPx = resources.getDimension(R.dimen.viewpager_current_item_horizontal_margin)
-        val pageTranslationX = nextItemVisiblePx + currentItemHorizontalMarginPx
+        val pageTranslationY = nextItemVisiblePx + currentItemHorizontalMarginPx
         val pageTransformer = ViewPager2.PageTransformer { page: View, position: Float ->
-            page.translationX = -pageTranslationX * position
+            page.translationY = pageTranslationY * position
 //            // Next line scales the item's height. You can remove it if you don't want this effect
-            page.scaleY = 1 - (0.25f * kotlin.math.abs(position))
+            page.scaleX = 1 - (0.25f * kotlin.math.abs(position))
 //            // If you want a fading effect uncomment the next line:
-             page.alpha = 0.25f + (1 - kotlin.math.abs(position))
+            page.alpha = 0.25f + (1 - kotlin.math.abs(position))
         }
-        viewPager.setPageTransformer(pageTransformer)
+        viewPagervertical.setPageTransformer(pageTransformer)
 //
 //// The ItemDecoration gives the current (centered) item horizontal margin so that
 //// it doesn't occupy the whole screen width. Without it the items overlap
@@ -61,10 +61,8 @@ class MainActivity : AppCompatActivity() {
             this,
             R.dimen.viewpager_current_item_horizontal_margin
         )
-        viewPager.addItemDecoration(itemDecoration)
+        viewPagervertical.addItemDecoration(itemDecoration)
 
 
     }
-
-
 }
